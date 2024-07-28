fa73467e4dd51f0f146a972e8c67e3d50497351f
Tal <talgg15xm@gmail.com>
2024-07-28 12:19:51 +0300

    fixed to live chat

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,5 +10,8 @@ async function bootstrap() {
     credentials: true,
   });
   await app.listen(3000);
+  app.useWebSocketAdapter(new IoAdapter(app));
+
 }
+
 bootstrap();
